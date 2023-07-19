9523c2103645837c0cae7aacaf3b049d86c85879
Janez <lapajne.janez@gmail.com>
2023-07-19 16:49:37 +0200

    added property methods to geotiff

--- data_manager/geotiff.py
+++ data_manager/geotiff.py
@@ -1,22 +1,55 @@
+from functools import lru_cache
+
 import numpy as np
 from rioxarray._io import open_rasterio
 
 
 class GeotiffRaster:
-    def __init__(self, file_path):
-        self._raster = self.init_geotiff_raster(file_path)
+    DATA_COLUMN_NAME = "reflectance"
 
-    def init_geotiff_raster(self, file_path):
+    def __init__(self, file_path):
+        self._raster = self._init_geotiff_raster(file_path)
+
+    def __repr__(self):
+        return repr(self._raster)
+
+    def __str__(self):
+        return str(self._raster)
+
+    def __len__(self):
+        return len(self._raster)
+
+    def _init_geotiff_raster(self, file_path):
         raster = open_rasterio(file_path)
         raster = raster.squeeze().drop("spatial_ref").drop("band")
-        raster.name = "reflectance"
+        raster.name = self.DATA_COLUMN_NAME
         return raster
 
+    @lru_cache(maxsize=None)
     def to_numpy(self, set_nodata_to_zero=True):
         array = self._raster.to_numpy()
         if set_nodata_to_zero:
-            array[np.where(array == self._raster._FillValue)] = 0
+            array[np.where(array == self.nodata)] = 0
         return array
 
+    @lru_cache(maxsize=None)
     def to_dataframe(self):
-        return self._raster.to_dataframe().reset_index()
+        df = self._raster.to_dataframe().reset_index()
+        df = df[df[self.DATA_COLUMN_NAME] != self.nodata]
+        return df.reset_index(drop=True)
+
+    @property
+    def nodata(self):
+        return self._raster._FillValue
+
+    @property
+    def file(self):
+        return self._raster
+
+    @property
+    def file_path(self):
+        return self._raster.encoding["source"]
+
+    @property
+    def shape(self):
+        return self._raster.shape
