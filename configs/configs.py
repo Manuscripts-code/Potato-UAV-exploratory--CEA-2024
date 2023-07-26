27f4ce464fe1b16c0b706362e5d7f4fb8dc1ac9a
Janez <lapajne.janez@gmail.com>
2023-07-26 17:05:13 +0200

    multispectral loader implemented

--- configs/configs.py
+++ configs/configs.py
@@ -1,11 +1,13 @@
 import os
 from pathlib import Path
 
+from utils.utils import read_toml
+
 # PATHS
 BASE_DIR = Path(__file__).parent.parent.absolute()
 LOGS_DIR = Path(BASE_DIR, "logs")
 DATA_DIR = Path(BASE_DIR, "data")
-TOML_DIR = Path(BASE_DIR, "configs", "toml")
+TOML_DIR = Path(BASE_DIR, "configs", "specific")
 
 MUTISPECTRAL_DIR = Path(DATA_DIR, "multispectral_images")
 SHAPEFILES_DIR = Path(DATA_DIR, "shapefiles")
@@ -16,4 +18,4 @@ SAVE_MERGED_DIR = Path(SAVE_DIR, "merged")
 # DATA CONFIGS
 SAVE_COORDS = os.getenv("SAVE_COORDS", "false") == "true"
 NUM_CLOSEST_POINTS = int(os.getenv("NUM_CLOSEST_POINTS", 10))
-DATA_TOML_NAME = TOML_DIR / os.getenv("DATA_TOML_NAME", "testing.toml")
+CONFIGS_TOML = read_toml(TOML_DIR / os.getenv("DATA_TOML_NAME", "testing.toml"))
--- data_manager/loaders.py
+++ data_manager/loaders.py
@@ -1,13 +1,63 @@
+from itertools import product
+
 from configs import configs, specific_paths
-from utils.utils import read_toml
+from data_manager.geotiffs import MultiGeotiffRaster
+from data_manager.mergers import MultiRasterPointsMerger, RasterPointsMerger
+from data_manager.shapefiles import PointsShapefile
 
 
 class MultispectralLoader:
+    ROOT = "multispectral"
+    DATES = "imagings_dates"
+    TREATMENTS = "treatments"
+    CHANNELS = "channels"
+    LOCATION_TYPE = "location_type"
+
     def __init__(self):
         self.rasters_paths = specific_paths.PATHS_MULTISPECTRAL_IMAGES
         self.shapefiles_paths = specific_paths.PATHS_SHAPEFILES
+        self.cfg = configs.CONFIGS_TOML[self.ROOT]
+
+        self.mergers = None
+        self.multi_merger = None
+
+    def load(self):
+        dates, treatments, channels, location_type = self._get_configs()
+        self.mergers = []
+        for date, treatment in product(dates, treatments):
+            merger = self._create_merger(date, treatment, channels, location_type)
+            self.mergers.append(merger)
+        return self
+
+    def run_merges(self):
+        if self.mergers is None:
+            raise ValueError("Mergers are not loaded.")
+        self.multi_merger = MultiRasterPointsMerger().add_mergers(self.mergers)
+        self.multi_merger.run_merges()
+        return self
+
+    def _get_configs(self):
+        try:
+            dates = self.cfg[self.DATES]
+            treatments = self.cfg[self.TREATMENTS]
+            channels = self.cfg[self.CHANNELS]
+            location_type = self.cfg[self.LOCATION_TYPE]
+        except KeyError:
+            raise KeyError("Missing key in config file.")
+        return dates, treatments, channels, location_type
+
+    def _create_merger(self, date, treatment, channels, location_type):
+        base_path = self.rasters_paths[treatment][date]
+        paths = {channel: base_path[channel] for channel in channels}
+        raster = MultiGeotiffRaster.from_paths(paths)
+        raster.set_name(date)
+
+        path_shape = self.shapefiles_paths[treatment][location_type]
+        shapefile = PointsShapefile.from_path(path_shape)
+
+        return RasterPointsMerger(raster, shapefile)
 
 
 if __name__ == "__main__":
-    s = read_toml("./configs/specific/testing.toml")
+    loader = MultispectralLoader().load().run_merges()
     pass
