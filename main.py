4511b18753a949adf6bb3bcd94b09944a9ecb8d4
Janez Lapajne <lapajne.janez@gmail.com>
2023-09-07 17:00:08 +0200

    added train and test to cmd options

--- main.py
+++ main.py
@@ -11,15 +11,36 @@ from pipelines.train import train_and_register_model_pipeline
 
 @click.command()
 @click.option(
+    "--config",
+    "-c",
+    type=click.Choice(
+        [configs.CMD_TRAIN_AND_REGISTER, configs.CMD_DEPLOY_AND_TEST, configs.CMD_EXECUTE_ALL]
+    ),
+    default=configs.CMD_EXECUTE_ALL,
+    help="Optionally you can choose to only run the deployment "
+    "pipeline to train and deploy a model (`deploy`), or to "
+    "only run a prediction against the deployed model "
+    "(`predict`). By default both will be run "
+    "(`deploy_and_predict`).",
+)
+@click.option(
     "--toml-config-file",
     default=configs.TOML_DEFAULT_FILE_NAME,
     type=str,
     help="Select among possible toml configs located in 'configs/specific/*.toml'",
 )
-def main(toml_config_file: str):
+def main(config: str, toml_config_file: str):
+    # Set the TOML config file as an environment variable (parsed in the pipelines)
     os.environ[configs.TOML_ENV_NAME] = toml_config_file
-    train_and_register_model_pipeline()
-    deployment_inference_pipeline()
+
+    do_train_and_register = config == configs.CMD_TRAIN_AND_REGISTER or config == configs.CMD_EXECUTE_ALL
+    do_deploy_and_test = config == configs.CMD_DEPLOY_AND_TEST or config == configs.CMD_EXECUTE_ALL
+
+    if do_train_and_register:
+        train_and_register_model_pipeline()
+
+    if do_deploy_and_test:
+        deployment_inference_pipeline()
 
     print(
         "\nYou can run:\n "
