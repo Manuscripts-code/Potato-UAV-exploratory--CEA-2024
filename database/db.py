89162dd478eddfd5cfccbc3def9a2d9ada61323b
Janez Lapajne <lapajne.janez@gmail.com>
2023-10-04 15:29:52 +0200

    decorator added to db calls

--- database/db.py
+++ database/db.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from sqlmodel import Session, SQLModel, create_engine
 
 from configs import configs
@@ -11,46 +13,24 @@ class SQLiteDatabase:
         sqlite_url = f"sqlite:///{configs.DB_PATH}"
         self.engine = create_engine(sqlite_url, echo=configs.DB_ECHO)
         SQLModel.metadata.create_all(self.engine)
+        self.session = None
 
-    def save_record(self, record: RecordSchema):
-        with Session(self.engine) as session:
-            session.add(record)
-            session.commit()
-            session.refresh(record)
+    def _with_session(no_autoflush=False):
+        def decorator(func):
+            @wraps(func)
+            def wrapper(self, *args, **kwargs):
+                if no_autoflush:
+                    with Session(self.engine).no_autoflush as session:
+                        self.session = session
+                        return func(self, *args, **kwargs)
+                else:
+                    with Session(self.engine) as session:
+                        self.session = session
+                        return func(self, *args, **kwargs)
 
-    def get_record(self, model_name: str, model_version: str):
-        with Session(self.engine) as session:
-            record = (
-                session.query(RecordSchema)
-                .filter(RecordSchema.model_name == model_name)
-                .filter(RecordSchema.model_version == model_version)
-                .first()
-            )
-            return record
+            return wrapper
 
-    def reset_latest_record(self, model_name: str):
-        with Session(self.engine) as session:
-            (
-                session.query(RecordSchema)
-                .filter(RecordSchema.model_name == model_name)
-                .filter(RecordSchema.is_latest == True)  # noqa: E712
-                .update({"is_latest": False})
-            )
-            session.commit()
-
-    def get_all_records(self):
-        with Session(self.engine).no_autoflush as session:
-            records = session.query(RecordSchema).all()
-            records = self._modify_records(records)
-            return records
-
-    def get_latest_records(self):
-        with Session(self.engine).no_autoflush as session:
-            records = (
-                session.query(RecordSchema).filter(RecordSchema.is_latest == True).all()  # noqa: E712
-            )
-            records = self._modify_records(records)
-            return records
+        return decorator
 
     def _modify_records(self, records: list[RecordSchema]):
         for record in records:
@@ -58,4 +38,44 @@ class SQLiteDatabase:
                 data.content = StructuredData.from_bytes(data.content)
             for prediction in record.predictions:
                 prediction.content = Prediction.from_bytes(prediction.content)
+        return records
+
+    @_with_session()
+    def save_record(self, record: RecordSchema):
+        self.session.add(record)
+        self.session.commit()
+        self.session.refresh(record)
+
+    @_with_session()
+    def get_record(self, model_name: str, model_version: str):
+        record = (
+            self.session.query(RecordSchema)
+            .filter(RecordSchema.model_name == model_name)
+            .filter(RecordSchema.model_version == model_version)
+            .first()
+        )
+        return record
+
+    @_with_session()
+    def reset_latest_record(self, model_name: str):
+        (
+            self.session.query(RecordSchema)
+            .filter(RecordSchema.model_name == model_name)
+            .filter(RecordSchema.is_latest == True)  # noqa: E712
+            .update({"is_latest": False})
+        )
+        self.session.commit()
+
+    @_with_session(no_autoflush=True)
+    def get_all_records(self):
+        records = self.session.query(RecordSchema).all()
+        records = self._modify_records(records)
+        return records
+
+    @_with_session(no_autoflush=True)
+    def get_latest_records(self):
+        records = (
+            self.session.query(RecordSchema).filter(RecordSchema.is_latest == True).all()  # noqa: E712
+        )
+        records = self._modify_records(records)
         return records
