04c4974990648f0a3547373b84b02ddbf8467280
janezlapajne <lapajne.janez@gmail.com>
2024-03-11 10:47:29 +0100

    Update run.sh script with new commands

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# python3 main.py --toml-config-file clf/alternaria.toml --results;
+
+# python3 main.py --toml-config-file reg/E.toml --results;
+# python3 main.py --toml-config-file reg/ETR.toml --results;
+# python3 main.py --toml-config-file reg/gsw.toml --results;
+# python3 main.py --toml-config-file reg/PhiPS2.toml --results;
+# python3 main.py --toml-config-file reg/SPAD.toml --results;
+# python3 main.py --toml-config-file clf/varieties_8_clf.toml --results;
 # python3 main.py --toml-config-file clf/alternaria_b.toml --results;
 
-python3 main.py --toml-config-file reg/E.toml;
-python3 main.py --toml-config-file reg/ETR.toml;
-python3 main.py --toml-config-file reg/gsw.toml;
-python3 main.py --toml-config-file reg/PhiPS2.toml;
-python3 main.py --toml-config-file reg/SPAD.toml;
+# python3 main.py -c train -t clf/alternaria_b.toml 
+# python3 main.py -c test -t clf/alternaria_test.toml -r
